aef3e5b1c01be34ea6bb1cc54967a13639b38fad
Oksana Patsii <oksanapatsii@gmail.com>
2025-08-18 14:55:18 +0200

    les27Home

---
+++ src/les27/home/Main.java
@@ -0,0 +1,19 @@
+package les27.home;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] num = new int[10][10];
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[i].length; j++) {
+                num[i][j] = i * j;
+            }
+        }
+
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[i].length; j++) {
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
